e5bb659ad218e48c172e68e011914eed6e8f8131
c-puma <cpumal@unsa.edu.pe>
2022-06-10 04:00:35 -0500

    Cree un super usuerio de nuevo,  aplique migrate

--- src/personas/models.py
+++ src/personas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-nombre = models.CharField(nax_length = 100)
-apellidos = models.CharField(nax_Length = 100)
-edad = models.IntegerField()
+    nombre = models.CharField(max_length = 100)
+    apellidos = models.CharField(max_length = 100)
+    edad = models.IntegerField()
