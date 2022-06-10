464779323ff464542af9e43010ff73c4fdf7c24f
c-puma <cpumal@unsa.edu.pe>
2022-06-10 03:26:47 -0500

    Cree mi modelo para mi app animales y lo inclui en settings

--- src/animales/models.py
+++ src/animales/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Animal(models.Model):
+    nombres = models.TextField()
+    edad = models.IntegerField()
+    salvaje = models.BooleanField()
