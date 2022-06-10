95e0deb8fb342525cbc9d7c2163aab9f8b544ab2
c-puma <cpumal@unsa.edu.pe>
2022-06-10 03:51:29 -0500

    Retrocedi en el tiempo y cree una nueva rama donde actualice el modelo de Persona

--- src/personas/models.py
+++ src/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+nombre = models.CharField(nax_length = 100)
+apellidos = models.CharField(nax_Length = 100)
+edad = models.IntegerField()
