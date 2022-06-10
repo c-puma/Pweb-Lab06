899b401232483a8e730f5e80fc8b9bde0d0cbb57
c-puma <cpumal@unsa.edu.pe>
2022-06-10 05:41:31 -0500

    Explorando los argumentos de las gunciones de las vistas

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -1,9 +1,13 @@
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def myHoneView(*args, **kwargs):
-    return HttpResponse('<hl>Hola Mundo desde Dlango</hi>')
+def myHoneView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return HttpResponse('<hl>Hola mundo desde Dlango</hi>')
+
 def anotherView(request):
     return HttpResponse('<hl>Solo es otra pagina desde Dlango</hi>')
 def testView(request):
